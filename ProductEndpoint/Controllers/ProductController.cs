f585bdd6337ab8a53c020c4f54aa466d83e45972
Prashant Vyas <Prashant.Vyas@LeadVenture.com>
2025-11-24 11:28:55 +0530

    Add project files.

---
+++ ProductEndpoint/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProductEndpoint.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ProductController : Controller
+	{
+		[HttpGet]
+		public IActionResult GetProducts()
+		{
+			var products = new List<object>
+			{
+				new { id = 1, name = "Laptop", price = 50000 },
+				new { id = 2, name = "Phone", price = 20000 }
+			};
+
+			return Ok(products);
+		}
+	}
+}
